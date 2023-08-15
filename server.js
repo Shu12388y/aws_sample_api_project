43f5dcf2d28ea5894ab569394260b0697e6b9c4e
Shu12388y <spl.sp999@gmail.com>
2023-08-15 21:48:44 +0530

    file added

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const PORT=3000
+const path=require("path")
+const fs=require("fs")
+const data=require("./data/product.json")
+
+
+app.get("/",(req,res)=>{
+    const homePage=fs.readFile(path.join(__dirname,"public","index.html"),"utf8",(error,content)=>{
+        if(error){
+            throw error
+        }
+        else{
+            
+            res.send(content)
+        }
+    })
+})
+
+
+
+app.get("/product",(req,res)=>{
+    res.json(data)
+
+})
+
+app.listen(PORT,()=>{
+    console.log("server is on")
+})
+
+
+// listen to port 3000
